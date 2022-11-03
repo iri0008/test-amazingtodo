a4712adce2061f7ee1cbae71734fba9b9edc9492
Ingrid Rismyhr <ingridrismyhr@Ingrids-MacBook-Pro-2.local>
2022-11-03 14:50:38 +0100

    fikset mye forskjellig

--- src/App.js
+++ src/App.js
@@ -1,35 +1,46 @@
 import './App.css';
 import { TextField } from "@mui/material";
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import React from 'react';
 import {Button, ListItemSecondaryAction} from "@mui/material";
 
-const myList = ["bade", "dusje"];
+let counter = 0;
+const css = {
+  input: { color: "#fff" }
+};
+
+const myList = [];
 
 function App() {
 
-  const [item, setItem] = useState(myList);
+  const [items, setItems] = useState(myList);
+  const [input, setInput] = useState("");
+
+var inputRef = useRef();
 
   const addItem = () => {
-  var newItem = document.getElementById("outlined-basic").value;
-    setItem(newItem);
-    myList.push(newItem);
-    console.log(myList);
+    //var newItem2 = document.getElementById("outlined-basic").value;
+    console.debug(inputRef);
+    const updatedList = [...items, {id: counter, item: input}]; //spread operator
+    setItems(updatedList);
+    counter++;
   }
+  //console.debug(items);
 
   return (
     <div className="App">
       <header className="App-header">  
 
         <div>
-          <TextField id="outlined-basic" label="Add an item here.." variant="outlined" />
+          <TextField id="outlined-basic" sx={css} value={input} onChange={e => setInput(e.target.value)} label="Add an item here.." variant="outlined" />
           <Button onClick={addItem} variant="contained">Add item </Button>
         </div>
 
-        <p> Oppgave lagt til: {item} </p>
-        
-        {myList.map((item) => (
-          <li>{item}</li>
+
+        {items.map((e) => (
+          <div>
+            <li key={e.id}>{e.item}</li>
+            </div>
         ))}
 
       </header>
