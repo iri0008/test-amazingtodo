0dffbe3d4473297905abee573b5dc89671ca98d6
Aurora Alveberg <aurora.alveberg@politiet.no>
2022-11-03 12:54:16 +0100

    lagt til liste

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const myList = ["bade", "dusje"];
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <button>Click me</button>
+        
+        {myList.map((myList) => <li>{myList}</li>)}
+
       </header>
     </div>
   );
